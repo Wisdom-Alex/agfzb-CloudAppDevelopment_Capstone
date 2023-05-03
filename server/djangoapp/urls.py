612f734bc4f038a42f0a1a34a0ff634a7ede072c
Wisdom-Alex <wizzyalex132@gmail.com>
2023-05-03 16:36:43 +0100

    First Work

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,24 +5,27 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
+                  # route is a string contains a URL pattern
+                  # view refers to the view function
+                  # name the URL
 
-    # path for about view
+                  # path for about view
 
-    # path for contact us view
+                  path(route='about/', view=views.about, name='about_us'),
 
-    # path for registration
+                  # path for contact us view
+                  path(route='contact/', view=views.contact, name='contact_us'),
 
-    # path for login
+                  # path for registration
 
-    # path for logout
+                  # path for login
 
-    path(route='', view=views.get_dealerships, name='index'),
+                  # path for logout
 
-    # path for dealer reviews view
+                  path(route='', view=views.get_dealerships, name='index'),
 
-    # path for add a review view
+                  # path for dealer reviews view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # path for add a review view
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000/djangoapp/' 'localhost']
 
 
 # Application definition
@@ -123,5 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+ Path(BASE_DIR, 'CheckStatic/static/')
+    ]
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
